6ec9059ce83eaef4698c6b4bbf91b1af605f612c
dewakkl <maladewaadrian12@gmail.com>
2025-05-28 16:35:47 +0700

    Create ContentPage.java

---
+++ Tubes_PBO/src/main/java/com/mycompany/tubes_pbo/ContentPage.java
@@ -0,0 +1,7 @@
+public interface ContentPage {
+    void showPage();             // Menampilkan halaman movie
+    void showTopRated();         // Menampilkan movie dengan rating tertinggi
+    void showMostReviewed();     // Menampilkan movie dengan jumlah review terbanyak
+    void showByGenre();          // Menampilkan movie berdasarkan genre
+    void everyRating();          // Menampilkan semua review & rating movie
+}
