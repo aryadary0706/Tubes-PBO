3729fa8c2e811323c757ad8e2052c647f5593045
dewakkl <maladewaadrian12@gmail.com>
2025-05-28 16:23:23 +0700

    Update Rating.java

--- Tubes_PBO/src/main/java/com/mycompany/tubes_pbo/Rating.java
+++ Tubes_PBO/src/main/java/com/mycompany/tubes_pbo/Rating.java
@@ -15,14 +15,22 @@ class Rating {
     private int directingRate;
     private int expectationScore;
     private float avgRate;
+    /*tambah*/
+    private String nama;
+    private String reviewText;
+    private int score;
 
-    public Rating(int story, int acting, int visual, int directing, int expectation) {
+    public Rating(int story, int acting, int visual, int directing, int expectation, String nama, String reviewText, int score) {
         this.storyRate = story;
         this.actingRate = acting;
         this.visualRate = visual;
         this.directingRate = directing;
         this.expectationScore = expectation;
         this.avgRate = countAvg();
+        /*tambah*/
+        this.nama = nama;
+        this.reviewText = reviewText;
+        this.score = score;
     }
 
     private float countAvg() {
@@ -32,4 +40,31 @@ class Rating {
     public float getAvgRate() {
         return avgRate;
     }
+
+    // Getter
+    public String getNama() {
+        return nama;
+    }
+
+    public String getReviewText() {
+        return reviewText;
+    }
+
+    public int getScore() {
+        return score;
+    }
+
+    // Setter
+    public void setNama(String nama) {
+        this.nama = nama;
+    }
+
+    public void setReviewText(String reviewText) {
+        this.reviewText = reviewText;
+    }
+
+    public void setScore(int score) {
+        this.score = score;
+    }
+}
 }
